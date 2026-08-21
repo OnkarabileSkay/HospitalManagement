ce27d47b7a86f3ce5be3f15af338e8ca3c27de5c
OnkarabileSkay <onkarabilesk@icloud.com>
2026-08-21 21:17:11 +0200

    J Unit Test

--- test/medicare/HospitalManagementSystemTest.java
+++ test/medicare/HospitalManagementSystemTest.java
@@ -38,7 +38,7 @@ public class HospitalManagementSystemTest {
 
         Patient found = null;
         for (Patient p : MediCare.patientsRecord) {
-            if (p.getPatientID().equalsIgnoreCase("P104")) {
+            if (p.getPatientID().equalsIgnoreCase("P102")) {
                 found = p;
                 break;
             }
